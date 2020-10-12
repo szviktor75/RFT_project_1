e4bd5dcd2aaa1f1875b30f56459b33ccf4d9ba8c
Szőllősi Viktor <szollosiviktor1975@gmail.com>
2020-10-12 10:42:12 +0200

    Négyzet osztály létrhozva.

---
+++ src/rft_procejt_1/Negyzet.java
@@ -0,0 +1,25 @@
+
+package rft_procejt_1;
+
+/**
+ *
+ * @author szollosiviktor
+ */
+public class Negyzet {
+    
+    private double oldal;
+    
+    public Negyzet(double oldal){
+        this.oldal = oldal;
+    }
+
+    public double getOldal() {
+        return oldal;
+    }
+
+    public void setOldal(double oldal) {
+        this.oldal = oldal;
+    }
+    
+    
+}
